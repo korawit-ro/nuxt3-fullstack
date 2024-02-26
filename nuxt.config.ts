b2fab1798fbc67d4b8d756b5dbf7c555d9ddd669
Korawit Romsuk <korawit.ro@outlook.com>
2024-02-26 15:13:37 +0700

    #feat: add nuxt image

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@element-plus/nuxt'],
+  modules: ['@nuxtjs/tailwindcss', '@element-plus/nuxt', '@nuxt/image'],
+  image: {
+    domains: [],
+  },
 });
