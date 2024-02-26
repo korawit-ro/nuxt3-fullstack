4e22681b0615ac9b0da90007c3ab3985e6e77801
Korawit Romsuk <korawit.ro@outlook.com>
2024-02-26 16:12:51 +0700

    #feat: add authJs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,19 @@
+import { resolve } from 'node:path';
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  alias: {
+    cookie: resolve(__dirname, 'node_modules/cookie'),
+  },
+  runtimeConfig: {
+    authJs: { secret: '/OEjlRC2DK74ZEj5nl8qHNy+E6/JptnouIyHnANbBz0=' },
+    public: {
+      apiBase: '/api',
+      authJs: {
+        baseUrl: 'http://localhost:3000',
+        verifyClientOnEveryRequest: true,
+      },
+    },
+  },
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss', '@element-plus/nuxt', '@nuxt/image', 'nuxt-mongoose'],
   image: {
@@ -9,5 +23,8 @@ export default defineNuxtConfig({
     uri: 'mongodb://localhost:27017/nuxt_db?authSource=admin',
     options: {},
     modelsDir: 'models',
+  },
+  authJs: {
+    guestRedirectTo: '/',
   },
 });
